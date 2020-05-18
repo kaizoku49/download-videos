d15248d17338e63d9aa1a8d439b4f71ef742d729
Tapan Pareek <tapanpareek20@gmail.com>
2020-05-18 14:03:11 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,28 @@
+ #!/system/bin/sh
+ echo "Starting the script..."
+cd ..
+cd .. 
+pkg install python -y
+
+pip install youtube-dl -y
+
+pip install --upgrade pip
+mkdir .config
+
+cd .config
+
+mkdir youtube-dl
+
+cd youtube-dl
+
+echo "-f dash-VIDEO-1+bestaudio/dash-VIDEO-1/best -o /storage/emulated/0/download_vids/%(title)s.%(ext)s" >> config 
+cd ..
+cd ..
+
+mkdir bin
+cd bin 
+ 
+echo 'youtube-dl $1' >> termux-url-opener
+
+
+echo "Congrats the process has now been completed" 
