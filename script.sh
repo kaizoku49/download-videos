b03ba477e921bdbdbcba391ae4bbf6ceaf61650e
Tapan Pareek <tapanpareek20@gmail.com>
2021-05-12 13:03:39 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,33 +1,18 @@
  #!/system/bin/sh
 echo "Starting the script..."
-cd ..
-cd .. 
-pkg update && pkg upgrade 
-echo "Downloading python, ffmpeg, curl..."
+cd ../..
+pkg update && pkg upgrade -y
 pkg install python -y
 pkg install curl -y 
 pkg install ffmpeg -y
-echo "Installing Youtube-dl..."
 pip install youtube-dl
-echo "Upgrading Pip..."
-pip install --upgrade pip
-echo "Making necessary files..."
 mkdir .config
-
 cd .config
-
 mkdir youtube-dl
-
 cd youtube-dl
-
 echo "-f dash-VIDEO-1+bestaudio/dash-VIDEO-1/best -o /storage/emulated/0/download_vids/%(title)s.%(ext)s" >> config 
-cd ..
-cd ..
-
+cd ../..
 mkdir bin
-cd bin 
- 
+cd bin  
 echo 'youtube-dl $1' >> termux-url-opener
-
-
 echo "Congrats the process has now been completed" 
